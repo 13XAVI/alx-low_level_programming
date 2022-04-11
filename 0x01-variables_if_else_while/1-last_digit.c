b3be78fc25edbef016869b4fb41588d5f005b55d
tresor xavier <tresorxavier16@gmail.com>
2022-04-11 09:11:17 -0700

    my message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ srand(time(0));
 n = rand()-RAND_MAX / 2;
 if(n < 6)
 {
-printf("%d is less than 6 and not 0", n);
+printf("%lu is less than 6 and not 0", n);
 }
 else if(n > 6)
 {
-printf("%d is greater than 6 and not 0", n);
+printf("%lu is greater than 6 and not 0", n);
 }
 else 
 {
-printf("%d is 0 and is 0");
+printf("%lu is 0 and is 0");
 }
 return(0);
 }
