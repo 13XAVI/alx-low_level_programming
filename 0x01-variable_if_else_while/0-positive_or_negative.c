9ced18d7838f43594e8fde3d900407017eb31e05
tresor xavier <tresorxavier16@gmail.com>
2022-04-11 00:25:27 -0700

    last change

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -17,12 +17,12 @@ if (n==0)
 printf("%i is zero\n", n);
 }
 else if(n<0)
-{ printf("%i is negative\n", n);
+{
+printf("%i is negative\n",n);	
 }
 else
 {
-printf("%i is positive");
-printf("%i is positive",n);
+printf("%i is positive\n",n);
 }
-return (0) ;
+return (0);
 }
