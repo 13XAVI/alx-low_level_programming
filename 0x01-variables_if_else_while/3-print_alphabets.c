d8a2ae5a6ae57b318c48f7ca82846f621a914302
tresor xavier <tresorxavier16@gmail.com>
2022-04-12 01:39:13 -0700

    my commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *
  *main - Entry point
+ *
  *Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 char upper_case;
 char lower_case;
@@ -14,7 +14,7 @@ for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 putchar(lower_case);
 }
 
-for (upper_case ='A'; upper_case <= 'Z'; upper_case++)
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
 putchar(upper_case);
 }
