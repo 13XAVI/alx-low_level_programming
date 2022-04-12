bc504c3ef59825c9bb8ff38fa2b0d401857decd3
tresor xavier <tresorxavier16@gmail.com>
2022-04-12 11:17:08 -0700

    my message

---
+++ main.h
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ *main - entry point
+ *desription: printing _putchar
+ *Return: Always 0 (sucess)
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+return (0);
