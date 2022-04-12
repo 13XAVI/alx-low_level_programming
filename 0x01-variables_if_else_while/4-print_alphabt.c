bd8bbcc6aca4df0f1fb0c6a5a6e3f36a1e2c39f5
tresor xavier <tresorxavier16@gmail.com>
2022-04-12 02:33:19 -0700

    my commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,15 +13,14 @@ for  (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 
 {
 if  (alphabet == 'q')
-{
 continue;
-}
+
 else if (alphabet == 'e')
-{
 continue;
+
 putchar(alphabet);
 }
 putchar('\n');
 return (0);
-}
+
 }
