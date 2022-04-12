6b947843d461a44d8bfc41036c3f1f7b5ad37759
tresor xavier <tresorxavier16@gmail.com>
2022-04-12 08:10:39 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+/**
+ *_putchar - this function will print _putchar
+ *
+ *Return: Always 0 (sucess)
+ */
+
+int _putchar()
+{
+putchar("_putchar");
+putchar('\n');
+
+return (0);
+}
