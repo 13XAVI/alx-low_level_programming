c65686be94a12d8f697b70816503d4c6248789f3
tresor xavier <tresorxavier16@gmail.com>
2022-04-13 03:49:11 -0700

    my commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *description: print all possible combination of single digit number
+ *Return: Always 0 (sucesss)
+ */
+int main(void)
+{
+int digit;
+int n = 0;
+for (digit = 48; digit <= 57; digit++)
+{
+putchar(digit, );
+}
+if (digit ==57)
+{
+n = 36;
+putchar(n);
+}
+return (0);
+}
