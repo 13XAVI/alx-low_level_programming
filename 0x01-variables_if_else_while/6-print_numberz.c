e7d59cf6c299821ac98e881a849900c323a487b7
tresor xavier <tresorxavier16@gmail.com>
2022-04-13 02:57:40 -0700

    my message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+/**
+ *main - Entry point 
+ *description: printing single digit numberof base ten
+ *Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+int digit;
+for (digit = 48; digit <= 57; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
+}
