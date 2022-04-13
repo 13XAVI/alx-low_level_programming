8e5cd68634652638b195aafe2e34e38d34fae0ec
tresor xavier <tresorxavier16@gmail.com>
2022-04-13 06:52:23 -0700

    my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *description: print lower case alphabetes
+ *Return: Always 0 (sucess)
+ */
+void print_alphabet(void)
+{
+int alph;
+for (alph = 97; alph <= 122; alph++)
+{
+putchar(alph);
+}
+putchar('\n');
+return;
+}
