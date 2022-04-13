7458d9e4ba344872ff03201e61ebdb2d711a718f
tresor xavier <tresorxavier16@gmail.com>
2022-04-13 02:33:38 -0700

    my message

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *description: printing anumber in base 16
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char alph;
+for (alph = 48; alph <= 57; alph++)
+{
+putchar(alph);
+}
+for (alph = 97; alph <= 102; alph++)
+{
+putchar(alph);
+}
+putchar('\n');
+}
+
