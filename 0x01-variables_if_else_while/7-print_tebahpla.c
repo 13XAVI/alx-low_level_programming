6290b97420fb255eea72d768faf248491839e127
tresor xavier <tresorxavier16@gmail.com>
2022-04-13 03:19:02 -0700

    my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+/**
+ *main -Entry point
+ *description: print lower case alphabet in reverse
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int lower_case;
+for (lower_case = 172; lower_case >= 97; lower_case--)
+{
+putchar(lower_case);
+}
+putchar('\n');
+return (0);
+}
