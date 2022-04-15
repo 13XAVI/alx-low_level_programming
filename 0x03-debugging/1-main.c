800cf7fdc050d978fc8beba443126e1dda6dd1c7
tresor xavier <tresorxavier16@gmail.com>
2022-04-15 09:34:43 -0700

    my commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - causes of infinte loop
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+int i;
+printf("infinite loop incoming :(\n");
+
+i = 0;
+/*
+ *while (i < 10)
+ *{
+ *infinite loop - no increase of variable
+ *putchar(i);
+}
+*/
+printf("infinite loop avoided! \\o/\n");
+return (0);
+}
