d0b122d88a971e665d505acf52c6bf8cf430f02b
tresor xavier <tresorxavier16@gmail.com>
2022-04-15 09:43:49 -0700

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,10 +11,10 @@ printf("infinite loop incoming :(\n");
 
 i = 0;
 /*
- *while (i < 10)
- *{
- *infinite loop - no increase of variable
- *putchar(i);
+*while (i < 10)
+*{
+*infinite loop - no increase of variable
+*putchar(i);
 }
 */
 printf("infinite loop avoided! \\o/\n");
