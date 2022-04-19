3b13b829b911253b4ee7582de8a6d090056b169a
tresor xavier <tresorxavier16@gmail.com>
2022-04-19 05:43:22 -0700

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,12 +4,16 @@
  *
  *Return: 0
  */
+
 int main(void)
 {
 int i;
+
 printf("infinite loop incoming :(\n");
 
 i = 0;
+
+
 /*
 *while (i < 10)
 *[
@@ -17,6 +21,7 @@ i = 0;
 *putchar(i);
 ]
 */
+
 printf("infinite loop avoided! \\o/\n");
 return (0);
 }
