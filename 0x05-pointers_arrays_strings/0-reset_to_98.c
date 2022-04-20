1957483d8ce949d147f3e307465d37dc5ce4eef7
tresor xavier <tresorxavier16@gmail.com>
2022-04-19 23:43:19 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates a interger value to 98
+ *@n: a pointer to an int that will be updated
+ *
+ *Return: void thaat means incorrect
+ */
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
