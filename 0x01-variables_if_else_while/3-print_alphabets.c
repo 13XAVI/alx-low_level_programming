caedea15cf949c99833566b38e12107cdab228b1
13XAVI <tresorxavier16@gmail.com>
2022-07-07 17:52:05 +0200

    my message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,20 +2,22 @@
 /**
  * main - entry point
  *
- * Return: Always 0 
+ * Return: Always 0
 */
 int main(void)
 {
 char lc;
 char up;
-for (up = 'A'; up <= 'Z'; up++)
-{
-putchar(up);
-}
+
 for (lc = 'a' ; lc <= 'z'; lc++)
 {
 putchar(lc);
 }
+for (up = 'A'; up <= 'Z'; up++)
+{
+putchar(up);
+}
+
 putchar('\n');
 
 return (0);
