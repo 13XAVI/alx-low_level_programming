b0e3c3e6dba313da656f7cabeace4f96c4ce901f
13XAVI <tresorxavier16@gmail.com>
2022-07-07 22:48:51 +0200

    my message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,6 +10,9 @@ int i;
 int j;
 for (i = 48; i <= 57 ; i++)
 {
+putchar(i);
+}
+
 for (j = 48; j <= 57; j++)
 {
 putchar(j);
@@ -18,8 +21,7 @@ if (i == j)
 {
 continue;
 }
-putchar(i);
-}
+
 
 putchar('\n');
 
