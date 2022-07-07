4dc77daa3906364ab259d2bf92c1d7c5a318cd64
13XAVI <tresorxavier16@gmail.com>
2022-07-07 19:10:35 +0200

    my message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+char a = 'alphabet';
+int i;
+for (i = a; i < a; i--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
