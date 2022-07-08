06c6e0a6c5e3673a8745270d251710e2905eb424
13XAVI <tresorxavier16@gmail.com>
2022-07-08 21:50:30 +0200

    my message

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: a character to be checked
  * Return: return 0 or 1 depending on c
  */
-int _ isalpha(int c)
+int _isalpha(int c)
 {
 return ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
 }
