40a5da37b25f37662b6e64959b73b90972f8398b
13XAVI <tresorxavier16@gmail.com>
2022-07-17 12:25:08 +0200

    my message on 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @src: string to complete
  * Return: new concateneted string
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 int index = 0, dest_len = 0;
 while (dest[index++])
