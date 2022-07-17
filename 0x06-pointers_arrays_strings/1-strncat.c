3a64ed814e3ea1a0489cd1b53eb1a717f43b86e4
13XAVI <tresorxavier16@gmail.com>
2022-07-17 12:08:06 +0200

    my message on 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,14 +5,14 @@
  * the end of the string pointed to by @dest
  * @dest: string that will be appended
  * @src: string to be concateneted upon
- * Return: pointer to @dest
+ * Return: new concateneted string
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 int index =0, dest_len = 0;
 while (dest[index++])
 dest_len++;
-for (index = 0; scr[index]; index++)
+for (index = 0; scr[index] && index < n; index++)
 dest[dest_len++] = src[index];
 return (dest);
 }
