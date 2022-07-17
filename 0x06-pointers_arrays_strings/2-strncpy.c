145c7df9e3a37101f4f98542983f4b844392a577
13XAVI <tresorxavier16@gmail.com>
2022-07-17 12:41:17 +0200

    my message on 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strncpy - c function that copies a string, including the terminating null bytes
+ * @dest: buffer storing the string copy
+ * @src: the source string
+ * @n: max number of byte copied 
+ * Return: new concateneted string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+while (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = scr [i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
+
