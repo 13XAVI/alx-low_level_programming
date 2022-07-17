c7a4be6158dbefe206b6e88e47836e2229ee37ef
13XAVI <tresorxavier16@gmail.com>
2022-07-17 12:02:30 +0200

    my message on 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat -concatenates the string pointed to by @src to
+ * *_strcat -concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest
  * @dest: string that will be appended
  * @src: string to be concateneted upon
@@ -9,7 +9,7 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int index =0, dest_len = 0;
+int index = 0, dest_len = 0;
 while (dest[index++])
 dest_len++;
 for (index = 0; src[index]; index++)
