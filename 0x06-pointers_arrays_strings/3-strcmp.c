b95418a43936babf3c78fcb3f6d0279b4a847d99
13XAVI <tresorxavier16@gmail.com>
2022-07-18 13:44:33 +0200

    my message on 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@
  * 		returns negative number if si < s2
  * 		returns positive number if s1 > s2
  */
-char *_strcmp(char *s1, char *s2)
+int *_strcmp(char *s1, char *s2)
 {
 int i = 0, diff = 0;
 while (1)
