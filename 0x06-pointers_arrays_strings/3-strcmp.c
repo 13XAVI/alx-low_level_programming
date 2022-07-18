c7fb83139242daaeb1ff1d053bbc697bad0b79c9
13XAVI <tresorxavier16@gmail.com>
2022-07-18 13:47:57 +0200

    my message on 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strcmp - concatenates two string but add inputted number of bytes
+ * _strcmp - function that compare two strings
  * @s1: first string 
- * @src: second string
- * Return: new concateneted string
+ * @s2: second string
+ * Return:
  * 		returns zero if s1 == s2
  * 		returns negative number if si < s2
  * 		returns positive number if s1 > s2
  */
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 int i = 0, diff = 0;
 while (1)
