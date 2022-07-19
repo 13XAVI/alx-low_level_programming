7444bb36439bd94db277510e41714544c0d1bfd8
13XAVI <tresorxavier16@gmail.com>
2022-07-20 01:24:23 +0200

    My commit on 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,10 +13,10 @@ for (i = 0; *(s + i); i++)
 {
 for (j = 0; *(accept + j); j++)
 {
-if (*(s + i) == * (accept + j))
+if (*(s + i) == *(accept + j))
         break;
 }
-if (*(accept +j) != '\0')
+if (*(accept + j) != '\0')
  return (s + i);
 }
 return (0);
