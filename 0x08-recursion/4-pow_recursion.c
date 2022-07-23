cb22d72ea1b24db94b7d117510f0fd712855c417
13XAVI <tresorxavier16@gmail.com>
2022-07-23 18:16:08 +0200

    my commit on 4-powwq_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _pow_recursion - function to print a string in reverse
+ * @x: number
+ * @y: power
+ *
+ * Return: x to the pow of y
+ * -1 if x < 0
+ */
+int _pow_recursion(char *s)
+{
+if (y < 0)
+{
+return (-1);
+}
+if (y == 1)
+{
+return (1);
+}
+if (y == 1)
+{
+return (x * _pow_recursion(x, y -1));
+}
+}
+ 
