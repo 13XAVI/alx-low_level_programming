f4c85853eb0bf7c3f618b5d9600597c4b07b8f5d
13XAVI <tresorxavier16@gmail.com>
2022-07-27 10:10:13 +0200

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,41 +12,42 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-        char *nstr;
-        unsigned int len1, len2, size, i, j;
-        if (s1 == NULL)
-        {
-                s1 = "";
-        }
-	if (s2 == NULL)
-        {
-                s2 = "";
-        }
-	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-	size = len1 + len2;
-        nstr = malloc((sizeof(char) * size) + 1);
-        /*check malloc was successful*/
-        if (nstr == NULL)
-        {
-                return (NULL);
-        }
-        for (i = 0; i < len1; i++)
-        {
-                nstr[i] = s1[i];
-        }
-	j = 0;
-	while (i <= size)
-	{
-	nstr[i] = s2[j];
-		i++;
-		j++;
-	}
-        return (nstr);
+char *nstr;
+unsigned int len1, len2, size, i, j;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+len1 = 0;
+while (s1[len1] != '\0')
+len1++;
+len2 = 0;
+while (s2[len2] != '\0')
+len2++;
+size = len1 + len2;
+nstr = malloc((sizeof(char) * size) + 1);
+/*check malloc was successful*/
+if (nstr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < len1; i++)
+{
+nstr[i] = s1[i];
+}
+j = 0;
+while (i <= size)
+{
+nstr[i] = s2[j];
+i++;
+j++;
+}
+return (nstr);
 
 }
 
