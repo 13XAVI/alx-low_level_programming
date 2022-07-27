5aca660a82abd45a989a633a120fa1b5a5643be9
13XAVI <tresorxavier16@gmail.com>
2022-07-27 13:25:11 +0200

    task 7

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -40,9 +40,12 @@ while (*(str + len))
 	len++;
 words = wordnos(str);
 if (words == 0)
+{
 	return (NULL);
 matrix = (char **) malloc(sizeof(char *) * (words + 1));
+}
 if (matrix == NULL)
+
 	return (NULL);
 for(i = 0; i <=len; i++)
 {
