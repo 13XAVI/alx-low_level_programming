9ed990dc9ad692b4bb1c30e56383b29ed889f24c
13XAVI <tresorxavier16@gmail.com>
2022-07-27 12:30:13 +0200

    task 6

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,7 +17,19 @@ k = 0;
 if (ac == 0 || av == NULL)
 return (NULL);
 i = 0;
-if (ac == 0 || av == NULL)
+while (i < ac)
+{
+j = 0;
+while (av[i][j])
+{
+size++;
+j++;
+}
+size++;
+i++;
+}
+arg = malloc((sizeof(char) * size) + 1);
+if (arg == NULL)
 return (NULL);
 i = 0;
 while (i < ac)
