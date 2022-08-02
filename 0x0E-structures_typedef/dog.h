12da50b903b012e1784a6ffc3151ec373edb1c39
13XAVI <tresorxavier16@gmail.com>
2022-08-02 23:29:06 +0200

    my commit on dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef STRTYP
+#define STRTYP
+/**
+ * struct dog - structure of a dog.
+ * @age: age of dog
+ * @owner: ownwer of dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, char *owner);
+void print_dog(structdog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t);
+
+#endif /*STRTYP*/
