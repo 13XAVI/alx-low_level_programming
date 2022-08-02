f93ead7ade0e50614e007bc23c74f2bed3e5b7dc
13XAVI <tresorxavier16@gmail.com>
2022-08-02 23:31:42 +0200

    my commit on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, char *owner);
 void print_dog(structdog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t);
+void free_dog(dog_t *d);
 
 #endif /*STRTYP*/
